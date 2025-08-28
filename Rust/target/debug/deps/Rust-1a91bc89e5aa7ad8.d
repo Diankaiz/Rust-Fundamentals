1a55027c6a671f1e6ab1359db8b031b139f67144
Diankaiz <kaizerjandoc1110@gmail.com>
2025-08-29 04:12:00 +0800

    new file:   Rust/Cargo.lock 	new file:   Rust/Cargo.toml 	new file:   Rust/src/main.rs 	new file:   Rust/target/.rustc_info.json 	new file:   Rust/target/CACHEDIR.TAG 	new file:   Rust/target/debug/.cargo-lock 	new file:   Rust/target/debug/.fingerprint/Rust-1a91bc89e5aa7ad8/invoked.timestamp 	new file:   Rust/target/debug/.fingerprint/Rust-1a91bc89e5aa7ad8/output-bin-Rust 	new file:   Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.3dkjiql01h6th5ih020vabvav.1y5g77y.rcgu.o 	new file:   Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.6jguk0j56fejdj3j95323pnrz.1y5g77y.rcgu.o 	new file:   Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.6jhj82y73p3y5659wnl6ppwm8.1y5g77y.rcgu.o 	new file:   Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.8f3nt9y0le4srm22k2kz563q9.1y5g77y.rcgu.o 	new file:   Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.8gzdwb5xrijjclbjqj36hv0zq.1y5g77y.rcgu.o# 	new file:   Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.9uhml67uj8z9c909v8e04jv39.1y5g77y.rcgu.o 	new file:   Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.d 	new file:   Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.dfqzm75lj1gqqjmmc6jl7c0mb.1y5g77y.rcgu.o 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/3dkjiql01h6th5ih020vabvav.o 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/6jguk0j56fejdj3j95323pnrz.o 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/6jhj82y73p3y5659wnl6ppwm8.o 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/8f3nt9y0le4srm22k2kz563q9.o 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/8gzdwb5xrijjclbjqj36hv0zq.o 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/dep-graph.bin 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/dfqzm75lj1gqqjmmc6jl7c0mb.o 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/query-cache.bin 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84-0q18m6hr8rlg0exqs1b1nuubv/work-products.bin 	new file:   Rust/target/debug/incremental/Rust-1wlncisp7jnb9/s-halg8mm3u7-1uj0s84.lock

---
+++ Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.d
@@ -0,0 +1,5 @@
+/home/kaiz/Rust-Fundamentals/Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8.d: src/main.rs
+
+/home/kaiz/Rust-Fundamentals/Rust/target/debug/deps/Rust-1a91bc89e5aa7ad8: src/main.rs
+
+src/main.rs:
